44a73aec7b094ce5dcb421c1f2c9d766fc086e20
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-01-27 17:41:43 +0300

    Initial Commit

---
+++ ProjectEuler1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectEuler1
+//
+//  Created by Alexander Sobolev on 27.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
